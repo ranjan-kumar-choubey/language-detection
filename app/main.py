069e41037cc9e1503810823935cf28bf424c478e
Your Name <you@example.com>
2024-03-21 17:19:56 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model.model import predict_pipeline
+from model.model import __version__ as model_version
+
+
+app = FastAPI()
+
+
+class TextIn(BaseModel):
+    text: str
+
+
+class PredictionOut(BaseModel):
+    language: str
+
+
+@app.get("/")
+def home():
+    return {"health_check here": "OK", "model_version": model_version}
+
+
+@app.get("/test")
+def testFunction():
+    return {"test message": "hello hunyy bunny"}
+
+
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    language = predict_pipeline(payload.text)
+    return {"language":language}
