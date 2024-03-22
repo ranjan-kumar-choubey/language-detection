7283e037baa2813554ec1c69f4287029771350eb
Your Name <you@example.com>
2024-03-22 08:06:29 +0530

    docker file added

--- app/main.py
+++ app/main.py
@@ -31,6 +31,18 @@ def predict(payload: TextIn):
     language = predict_pipeline(payload.text)
     return {"language":language}
 
-
+# uncomment only for development , not for production
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, log_level="info")
+
+
+# sudo docker ps -a
+# sudo docker build -t language-detection-app .
+# sudo docker run -d --name test-add -p 80:80 language-detection-app
+# sudo docker rm <container_id>
+
+#sudo lsof -i -P -n | grep 8080
+#sudo kill -9 <portno>      ,or 
+# kill -9 $(lsof -t -i tcp:<port#>)
+# docker exec -it <container_id> bash //to enter into docker 
+# commnet
