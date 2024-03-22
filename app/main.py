a3316f0b45e337f12594c2f40830f16ba871cd92
Your Name <you@example.com>
2024-03-22 08:37:00 +0530

    flast testing

--- app/main.py
+++ app/main.py
@@ -1,39 +1,39 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# import uvicorn
-# from model.model import predict_pipeline
-# from model.model import __version__ as model_version
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+from model.model import predict_pipeline
+from model.model import __version__ as model_version
 
 
-# app = FastAPI()
+app = FastAPI()
 
 
-# class TextIn(BaseModel):
-#     text: str
+class TextIn(BaseModel):
+    text: str
 
 
-# class PredictionOut(BaseModel):
-#     language: str
+class PredictionOut(BaseModel):
+    language: str
 
 
-# @app.get("/")
-# def home():
-#     return {"health_check here": "OK", "model_version": model_version}
+@app.get("/")
+def home():
+    return {"health_check here": "OK", "model_version": model_version}
 
 
-# @app.get("/test")
-# def testFunction():
-#     return {"test message": "hello hunyy bunny"}
+@app.get("/test")
+def testFunction():
+    return {"test message": "hello hunyy bunny"}
 
 
-# @app.post("/predict", response_model=PredictionOut)
-# def predict(payload: TextIn):
-#     language = predict_pipeline(payload.text)
-#     return {"language":language}
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    language = predict_pipeline(payload.text)
+    return {"language":language}
 
-# # uncomment only for development , not for production
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", port=5000, log_level="info")
+# uncomment only for development , not for production
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
 
 
 # sudo docker ps -a
@@ -47,7 +47,7 @@
 # docker exec -it <container_id> bash //to enter into docker 
 # commnet
     
-import streamlit as st
+    import streamlit as st
 
 if not hasattr(st, 'already_started_server'):
     # Hack the fact that Python modules (like st) only load once to
